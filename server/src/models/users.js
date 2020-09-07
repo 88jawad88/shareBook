fe25a4851119797b4588cdb3f008ea765016f932
jawad <jawad88halawa@gmail.com>
2020-09-07 18:33:46 +0300

    adding hash to password for users

--- server/src/models/users.js
+++ server/src/models/users.js
@@ -1,12 +1,27 @@
 //import the mongoose library
 const { Schema, model } = require("mongoose");
 
+const { hashPassword } = require("../../helpers");
+
+const shortId = require("shortid");
+
 //defining your client schema
 const userSchema = new Schema({
   firstName: String,
   lastName: String,
   email: { type: String, unique: true },
   password: String,
+});
+// تخزين كلمة السر بعد عمل الهاش
+
+userSchema.pre("save", function (next) {
+  if (!this.salt) {
+    this.salt = shortId.generate();
+  }
+  if (this.password) {
+    this.password = hashPassword(this.password, this.salt);
+  }
+  next();
 });
 
 //create  the user model and export
